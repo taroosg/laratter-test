507e49e8740d5bd9f921bbe8ba542ce338eae80b
taroosg <root@taroosg.dev>
2023-12-05 21:17:27 +0900

    バリデーション結果を表示するよう修正

--- resources/views/tweets/create.blade.php
+++ resources/views/tweets/create.blade.php
@@ -16,6 +16,9 @@
             <div class="mb-4">
               <label for="tweet" class="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2">Tweet</label>
               <input type="text" name="tweet" id="tweet" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+              @error('tweet')
+              <span class="text-red-500 text-xs italic">{{ $message }}</span>
+              @enderror
             </div>
             <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Tweet</button>
           </form>
