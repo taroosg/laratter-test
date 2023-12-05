2bdae1d94d32b622b615f7ae9f077517b893a2c9
taroosg <root@taroosg.dev>
2023-12-05 20:46:42 +0900

    TweetのルーティングとApiコントローラを追加

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\TweetController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +22,8 @@ Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logo
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
   return $request->user();
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+  Route::apiResource('tweets', TweetController::class);
 });
